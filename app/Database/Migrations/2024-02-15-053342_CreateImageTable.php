e64c3904259fd73d3d9d2f207ec602752d6b5062
AlexisNarvaez00 <zaizealexis@gmail.com>
2024-02-15 00:04:51 -0600

    ft: migrate file added

--- app/Database/Migrations/2024-02-15-053342_CreateImageTable.php
+++ app/Database/Migrations/2024-02-15-053342_CreateImageTable.php
@@ -9,11 +9,18 @@ class CreateImageTable extends Migration
   public function up()
   {
     $this->forge->addField([
+      "id" => [
+        "type" => "serial",
+        "null" => false,
+      ]
+    ]);
+    $this->forge->addField([
       "name" => [
         "type" => "text",
         "null" => false,
       ]
     ]);
+    $this->forge->addPrimaryKey("id");
     $this->forge->createTable("images");
   }
 
